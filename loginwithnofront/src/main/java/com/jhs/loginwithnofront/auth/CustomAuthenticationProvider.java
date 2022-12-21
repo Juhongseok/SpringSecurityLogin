0728a5109cb393b331d1eff48be43d622f1ea2f0
JHS <wnghdtjr129@naver.com>
2022-12-21 16:28:44 +0900

    feat: Security 구성요소 구현
    - UserDetails: CustomUser
    - USerDetailsService: UserService
    - AuthenticationProvider: CustomAuthenticationProvider

---
+++ loginwithnofront/src/main/java/com/jhs/loginwithnofront/auth/CustomAuthenticationProvider.java
@@ -0,0 +1,39 @@
+package com.jhs.loginwithnofront.auth;
+
+import com.jhs.loginwithnofront.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@RequiredArgsConstructor
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getPrincipal().toString();
+        UserDetails user = userService.loadUserByUsername(username);
+
+        if (passwordEncoder.matches(password, user.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(
+                    user.getUsername(),
+                    user.getPassword(),
+                    user.getAuthorities()
+            );
+        }
+        throw new BadCredentialsException("BAD Credentials");
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.isAssignableFrom(UsernamePasswordAuthenticationToken.class);
+    }
+}
---
+++ loginwithnofront/src/main/java/com/jhs/loginwithnofront/service/UserService.java
@@ -0,0 +1,29 @@
+package com.jhs.loginwithnofront.service;
+
+import com.jhs.loginwithnofront.controller.SignUpRequest;
+import com.jhs.loginwithnofront.domain.CustomUser;
+import com.jhs.loginwithnofront.domain.User;
+import com.jhs.loginwithnofront.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByEmail(username);
+        return new CustomUser(user);
+    }
+
+    public void saveUSer(SignUpRequest request) {
+        User user = request.toEntity(passwordEncoder);
+        userRepository.save(user);
+    }
+}
