ef9e973048483e13cdb29ea191dd2ed37bc31fc4
JHS <wnghdtjr129@naver.com>
2022-12-21 15:48:38 +0900

    feat: 프론트 없이 백엔드만 사용하여 로그인 구현 프로젝트

---
+++ loginwithnofront/src/main/java/com/jhs/loginwithnofront/LoginwithnofrontApplication.java
@@ -0,0 +1,13 @@
+package com.jhs.loginwithnofront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginwithnofrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginwithnofrontApplication.class, args);
+	}
+
+}
