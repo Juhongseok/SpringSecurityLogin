04d572773f33719ff91924919e1fb31ddf2c7b03
JHS <wnghdtjr129@naver.com>
2022-12-26 18:33:31 +0900

    feat: json format login project start

---
+++ loginwithjson/src/main/java/com/jhs/loginwithjson/LoginwithjsonApplication.java
@@ -0,0 +1,13 @@
+package com.jhs.loginwithjson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginwithjsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginwithjsonApplication.class, args);
+	}
+
+}
